6a8dc32dee71bf6e509081ad4dacbdb4bcc95011
bosmanpa <bsmn.ptrck@gmail.com>
2020-03-11 23:35:08 -0500

    added account history

--- MySuperBank/Program.cs
+++ MySuperBank/Program.cs
@@ -6,8 +6,26 @@ namespace MySuperBank
     {
         static void Main(string[] args)
         {
-            var account = new BankAccount("Pat");
+            var account = new BankAccount("Pat", 10000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+
+            // Test that the initial balances must be positive.
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+            account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
         }
     }
 }
